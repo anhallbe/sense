ea163e9853c2af4145630dd8bb1573873571e2fb
Andreas Hallberg <anhallbe@gmail.com>
2015-04-03 15:15:35 +0000

    frontend search

--- senseServer/assets/js/app/app.js
+++ senseServer/assets/js/app/app.js
@@ -3,6 +3,21 @@ var app = angular.module('main', []);
 app.controller('mainController', ['$scope', '$log', function($scope, $log) {
   $scope.sensors = [];
 
+  $scope.search = function() {
+    $log.info("search " + $scope.query);
+    io.socket.get('/sensor/search?q=' + $scope.query, function(resData, jwres) {
+      $scope.sensors = [];
+      for(var r in resData) {
+        $scope.sensors.push({name:resData[r]._source.name, value:resData[r]._source.value, id:resData[r]._id});
+      }
+      $scope.$apply();
+    });
+  };
+
+  $scope.addSensor = function() {
+
+  }
+
   io.socket.on('sensor', function(obj) {
     if(obj.verb === 'created') {
       $log.info("CREATED");
@@ -13,7 +28,7 @@ app.controller('mainController', ['$scope', '$log', function($scope, $log) {
   io.socket.get('/sensor', function(resData, jwres) {
     $log.info(resData);
     for(var r in resData) {
-      $scope.sensors.push({name:resData[r]._source.name, value:resData[r]._source.value});
+      $scope.sensors.push({name:resData[r]._source.name, value:resData[r]._source.value, id:resData[r]._id, time:resData[r]._source.createdAt});
     }
     $scope.$apply();
   });
